f9dcee9f120a6ae13e97fcc92edee034c9bde69c
DavisDevelopment <epikit@gmail.com>
2020-01-05 22:12:45 -0600

    gitignore

---
+++ ql/sql/G.hx
@@ -0,0 +1,17 @@
+package ql.sql;
+
+import ql.sql.grammar.CommonTypes;
+import ql.sql.TsAst;
+
+/**
+	G - internal utility class which contains static members used as global constants/functions within this module
+**/
+class G {
+	// #if macro
+	// public static macro function buildNode(nodeType:SqlNodeType):Array<haxe.macro.Expr.Field> {
+	// 	var fields = Context.getBuildFields();
+
+	// 	return fields;
+	// }
+	// #end
+}
